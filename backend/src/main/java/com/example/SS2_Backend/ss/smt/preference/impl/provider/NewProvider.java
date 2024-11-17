4d93b62a857ea85692580af9beed8c8e89196262
suyttthideptrai <ntienthanhn@gmail.com>
2024-11-18 01:45:00 +0700

    remove files

--- backend/src/main/java/com/example/SS2_Backend/ss/smt/preference/impl/provider/NewProvider.java
+++
@@ -1,6 +0,0 @@
-package com.example.SS2_Backend.ss.smt.preference.impl.provider;
-
-import com.example.SS2_Backend.ss.smt.preference.PreferenceProvider;
-
-public class NewProvider implements PreferenceProvider {
-}
--- frontend/src/module/core/component/ErrorText/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./style.scss";
-
-export default function ErrorText() {
-    return (
-        <div className='error-text'>
-            <p>This field must not be empty!</p>
-        </div>
-    );
-}
